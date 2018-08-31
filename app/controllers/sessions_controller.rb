02710ae3885da9442b1ec28ca99e071e343cadcc
derrick b <dbozich1@gmail.com>
2018-08-31 22:00:27 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
   def create
     @user = User.find_by(:name => params[:name])
+    binding.pry
     if @user.authenticate(params[:password])
       redirect_to welcome_path
     else
