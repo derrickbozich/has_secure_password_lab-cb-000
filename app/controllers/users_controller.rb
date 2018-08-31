bb8a286ba8d4254501e829681ec28613403f25dd
derrick b <dbozich1@gmail.com>
2018-08-31 22:09:29 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
 
     @user = User.new(:name => params[:user][:name], :password => params[:user][:password], :password_confirmation => params[:user][:password_confirmation])
-    binding.pry
+    # binding.pry
     if @user.save
       session[:user_id] = @user.id
       redirect_to welcome_path
