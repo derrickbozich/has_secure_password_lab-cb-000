74ad58fd4149f7ba0dfd190f2085755147b5bf40
derrick b <dbozich1@gmail.com>
2018-08-31 21:42:48 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new(:name => params[:name], :password => params[:password])
+
+    if @user.save
+
+    else
+
+    end
+  end
+
+  def welcome
+    render :welcome
+  end
 end
