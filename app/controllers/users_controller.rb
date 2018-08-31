0a824a76396b6205a5afe6dafd31939c7ab914fe
derrick b <dbozich1@gmail.com>
2018-08-31 22:09:12 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def create
 
-    @user = User.new(:name => params[:user][:name], :password => params[:user][:password])
+    @user = User.new(:name => params[:user][:name], :password => params[:user][:password], :password_confirmation => params[:user][:password_confirmation])
     binding.pry
     if @user.save
       session[:user_id] = @user.id
